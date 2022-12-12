a5133d19e10414bed7fa69f45070402dda50d3c4
mohamud <mohmud141@gmail.com>
2022-12-12 07:13:45 +0300

    useEffect part two

--- src/Components/Database.js
+++ src/Components/Database.js
@@ -2,28 +2,37 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 function Database() {
-    const [posts, setPosts] = useState([])
+    const [post, setPost] = useState({})
+    const [id, setId] = useState(1)
+    const [buttonClick, setButtonClick] = useState(1)
+
+    const handleClick = () =>{
+        setButtonClick (id)
+    }
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
         .then(res =>{
             console.log(res)
-            setPosts(res.data)
+            setPost(res.data)
         })
         .catch(err =>(
             console.log(err)
         ))
-    }, [])    
+    }, [buttonClick])    
 
     
   
   return (
     <div>
-<ul>
+        <input type='text' value={id} onChange={e => setId(e.target.value)}/>
+        <button type='button' onClick={handleClick}>fetch post</button>
+        <div>{post.title}</div>
+{/* <ul>
             {posts.map(posts =>( 
                 <li key={posts.id}>{posts.title}</li>
             ))}
-        </ul>
+        </ul> */}
     </div>
   )
 }
